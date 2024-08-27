452064c165c215891b4a54b05a6625d3eb673f34
Stepina <enot5662@gmail.com>
2024-08-27 11:02:03 +0200

    add the fifth game
    implemented the logic of the game “Is the number prime?”
    added the script for the game
    created the asciinema record and added the link to readme.md
    added data to makefile

--- README.md
+++ README.md
@@ -3,10 +3,22 @@
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/85101bb581b9f2e9c59f/maintainability)](https://codeclimate.com/github/AnastasiaStepina/python-project-49/maintainability)
 
+### rec brain-calc
+
 [![asciicast](https://asciinema.org/a/NA6QyKYhn99Fr4dFGV73UBjj1.svg)](https://asciinema.org/a/NA6QyKYhn99Fr4dFGV73UBjj1)
+
+### rec brain-even
 
 [![asciicast](https://asciinema.org/a/Zj642UO8298gQq8XbmsnMEBcf.svg)](https://asciinema.org/a/Zj642UO8298gQq8XbmsnMEBcf)
 
+### rec brain-gcd
+
 [![asciicast](https://asciinema.org/a/JkK1rBdncIyEmwm3HBuenOmT0.svg)](https://asciinema.org/a/JkK1rBdncIyEmwm3HBuenOmT0)
 
+### rec brain-progression
+
 [![asciicast](https://asciinema.org/a/s2YeIYhPZAGa237xJtpqvgmUN.svg)](https://asciinema.org/a/s2YeIYhPZAGa237xJtpqvgmUN)
+
+### rec brain-prime
+
+[![asciicast](https://asciinema.org/a/A8F6uARAba0e3tJP2lE1HjGVm.svg)](https://asciinema.org/a/A8F6uARAba0e3tJP2lE1HjGVm)
---
+++ brain_games/games/prime.py
@@ -0,0 +1,40 @@
+import random
+from brain_games.games.even import welcome_user
+from brain_games.games.const import MIN_NUMBER, MAX_NUMBER, WRONG_ANSWER, PRIME
+
+
+def get_question():
+    num = random.randint(MIN_NUMBER, MAX_NUMBER)
+    print(f"Question: {num}")
+    return num
+
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    for num in range(2, number):
+        if number % num == 0:
+            return False
+    return True
+
+
+def main():
+    name = welcome_user()
+    print(PRIME)
+    correct_answers = 0
+    while correct_answers < 3:
+        num = get_question()
+        answer = input('Your answer: ')
+        if answer == 'yes' and is_prime(num) or answer == 'no' and not is_prime(num):
+            correct_answers += 1
+            print("Correct!")
+        else:
+            correct_answers = 0
+            correct_answer = 'yes' if is_prime(num) else 'no'
+            print(f"{answer} {WRONG_ANSWER} {correct_answer}.")
+            print(f"Let's try again, {name}!")
+    print(f"Congratulations, {name}!")
+
+
+if __name__ == '__main__':
+    main()
